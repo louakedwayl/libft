c7625c7f0cd33d3a12940cf9bbb5fc037ec974bf
wayl <louakedwayl@protonmail.com>
2024-05-25 22:39:48 +0200

    maj header

--- libft.h
+++ libft.h
@@ -41,5 +41,17 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);				//14
 void	*ft_calloc(size_t nmemb, size_t size);				//21
 char	*ft_strdup(const char *s);					//13:
+							
+char *ft_substr(char const *s, unsigned int start,size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_itoa(int n);
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
+char *ft_strtrim(char const *s1, char const *set);
+char **ft_split(char const *s, char c);
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
